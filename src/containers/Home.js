1b114720bb393dffb1af055b369f1c189fdf4e23
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2023-09-18 01:39:38 -0700

    fixed dependencies and incompatibilities that were stoping the app fromm loading

---
+++ src/containers/Home.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import {Link, withRouter} from "react-router-dom";
+// import { Link, withRouter, BrowserRouter } from "react-router-dom";
+// import {Router} from 'react-router';
+import * as data from '../assets/data';
+import ReactHtmlParser from 'react-html-parser';
+
+class Home extends Component{
+  constructor(props) {
+		super(props);
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      currentPanel: data.panels[0].id
+    };
+  }
+
+  handleClick = (panelId, e) => {
+    // console.log("state before", this.state);
+    this.setState({currentPanel: panelId});
+    // console.log("state after", this.state);
+  }
+
+  panelContainer = (panel) => {
+    // const panelActiveCheck = (this.state.currentPanel === panel.id){this.setState({panelActive:"active"})};
+    let html = [];
+
+    data.panels.map((panel) => {
+      const activeClassName = this.state.currentPanel === panel.id ? "active" : null;
+      const panelSelected = this.state.currentPanel === panel.id ? "showText" : "";
+
+      html.push(
+        <div className="panel" data-active={activeClassName} key={panel.id}>
+          <div className="panelContent" onClick={(e) => this.setState({currentPanel: panel.id})}>
+            <div className="panelHead">
+              <div className="panelTitle">{panel.title}</div>
+              <div className="panelFirstName">{panel.firstName}</div>
+              <div className="panelLastName">{panel.lastName}</div>
+            </div>
+            <div className={`panelText ${panelSelected}`}>
+              {ReactHtmlParser(panel.text)}
+            </div>
+            <Link to={`/${panel.link}`} >
+              <div className="linkText">
+                <span className='material-symbols-outlined'>arrow_forward</span>
+              </div>
+            </Link>
+          </div>
+        </div>
+        );
+      });
+      console.log('html outside map panelContainer::', html);
+      return html;
+  }
+
+  render() {
+    // const currentPanel = this.state.currentPanel;
+    return(
+      <div className="content-area">
+
+      <div className={`panel-content-area ${this.state.currentPanel}`}>
+          {console.log('this.state.currentPanel::', this.state.currentPanel)}
+          {this.panelContainer()}
+      </div>
+      </div>
+
+    );
+  };
+};
+
+export default withRouter(Home);
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../src/scss/styles.scss';
+import App from './containers/App';
+import * as serviceWorker from './serviceWorker';
+// import {Router} from 'react-router';
+import {BrowserRouter} from 'react-router-dom';
+import {createBrowserHistory} from "history";
+
+
+
+const history = createBrowserHistory({basename:"/2022-2023"});
+// const router = () => {<Router/>};
+
+ReactDOM.render(
+  <BrowserRouter basename="/2022-2023" history={history} >
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
